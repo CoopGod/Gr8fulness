8398d6372ade88fb0d596cfd3168bb29a2138e0a
Cooper Goddard <57197353+CoopGod@users.noreply.github.com>
2021-11-22 19:42:21 -0700

    Made database a secret (shhh)

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from werkzeug.utils import redirect
 app = Flask(__name__)
 app.secret_key = "oh_so_secret"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_DATABASE_URI"] = environ.get('SQLALCHEMY_DATABASE_URI')
+app.config["SQLALCHEMY_DATABASE_URI"] = environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 
 #class defining user writings/entries 
