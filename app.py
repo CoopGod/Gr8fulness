661182a2987a347b818deb5ed14ba15fd7db8bb6
Cooper Goddard <57197353+CoopGod@users.noreply.github.com>
2021-11-22 20:35:29 -0700

    Revert back to old Heroku setup

--- app.py
+++ app.py
@@ -16,7 +16,11 @@ from werkzeug.utils import redirect
 app = Flask(__name__)
 app.secret_key = "oh_so_secret"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_DATABASE_URI"] = str(os.environ['DATABASE_URL'])
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://wkplzdpcuvcdbp:a26cdeabd8551f1578d9be0e151c1afb88f469f315bf90bf5232073c49bdddcf@ec2-52-72-125-94.compute-1.amazonaws.com:5432/dfpu28ru0s3ujl'
+db = SQLAlchemy(app)
+
+#class defining user writings/entries 
+
 print(os.environ['DATABASE_URL'])
 db = SQLAlchemy(app)
 
